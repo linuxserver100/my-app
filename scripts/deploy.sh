816c14ce5360109f82571132016d5c26498dd8f6
linuxserver100 <linuxserverub@gmail.com>
2024-10-29 10:04:15 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ sudo rm -rf $FRONTEND_PATH/build
 sudo mv ~/frontend/build $FRONTEND_PATH
 # Backend deployment
 echo "Deploying backend..."
-sudo npm i pm2 -g && cd /var/www/backend && sudo pm2 start index.js
+sudo npm i pm2 -g && cd /var/www/myapp/backend && sudo pm2 start index.js
