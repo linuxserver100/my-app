46328e2739ad6c2236bc6934a20fc30f51a912d1
linuxserver100 <linuxserverub@gmail.com>
2024-10-29 10:02:19 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@ FRONTEND_PATH="/var/www/myapp/frontend"
 BACKEND_PATH="/var/www/myapp/backend"
 # Frontend deployment
 echo "Deploying frontend..."
-rm -rf $FRONTEND_PATH/build
-mv ~/frontend/build $FRONTEND_PATH
+sudo rm -rf $FRONTEND_PATH/build
+sudo mv ~/frontend/build $FRONTEND_PATH
 # Backend deployment
 echo "Deploying backend..."
-sudo npm i pm2 -g && cd /var/www/backend && pm2 start index.js
+sudo npm i pm2 -g && cd /var/www/backend && sudo pm2 start index.js
